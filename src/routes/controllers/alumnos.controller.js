6bb1bd53fef25697eef6d95db9f1e86b7913c883
Daniel S Amaya <daniel_amaya7398@hotmail.com>
2022-11-17 22:43:55 -0600

    New: delete an Alumno

--- src/routes/controllers/alumnos.controller.js
+++ src/routes/controllers/alumnos.controller.js
@@ -1,4 +1,4 @@
-const alumnos = [
+let alumnos = [
     { id: 1, nombre: "Daniel", apellido: "Garcia", matricula: "123456", promedio: 9.5 },
     { id: 2, nombre: "Juan", apellido: "Perez", matricula: "123457", promedio: 8.5 },
 ];
@@ -37,16 +37,32 @@ module.exports.createAlumno = (req, res) => {
 }
 
 module.exports.updateAlumno = (req, res) => {
+    const { id } = req.params;
+    const alumnoFound = searchById(id, alumnos);
+    if (!alumnoFound) {
+        return res.status(404).json({ "Error": "Alumno not found" });
+    }
+
+    const {
+        nombre = alumnoFound.nombre,
+        apellido = alumnoFound.apellido,
+        promedio = alumnoFound.promedio,
+        matricula = alumnoFound.matricula } = req.body;
+    console.log(alumnos[alumnos.indexOf(alumnoFound)]);
+    res.status(200).json({ nombre, apellido, promedio, matricula });
 
 }
 
 module.exports.deleteAlumno = (req, res) => {
-    res.json({
-        ok: true,
-        msg: 'deleteAlumno',
-        al: alumnos[0],
-        body: "deleted"
-    })
+    const { id } = req.params;
+    const alumnoFound = searchById(id, alumnos);
+    if (!alumnoFound) {
+        return res.status(404).json({ "Error": "Alumno not found" });
+    }
+    const newAlumnos = alumnos.filter(al => al.id != id);
+    alumnos = newAlumnos;
+    res.status(200).json({ "deleted": alumnoFound });
+
 }
 
 //module.exports = { getAlumnos }
