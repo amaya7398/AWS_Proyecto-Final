99c2bec7f8780a30d8534b8e34ddd654fbd2ea5e
Daniel S Amaya <daniel_amaya7398@hotmail.com>
2022-11-24 03:58:07 -0600

    Fix: endpoints de /profesores pasan los tests

--- src/routes/controllers/profesores.controller.js
+++ src/routes/controllers/profesores.controller.js
@@ -4,37 +4,28 @@ const searchById = (id, arreglo) => {
     return arreglo.find(a => a.id == id);
 }
 
+module.exports.methodNotAllowed = (_, res) => {
+    res.status(405).json({ "Error": "Method not allowed" });
+}
+
 module.exports.getProfesores = (_, res) => {
-    const dataProfesores = profesores.map(prof => {
-        const { numeroEmpleado, nombres, apellidos, horaClase } = prof;
-        return { numeroEmpleado, nombres, apellidos, horaClase };
-    });
-    res.status(200).json({
-        profesores: dataProfesores
-    });
+    res.status(200).json({ profesores });
 }
 
 module.exports.getProfesorById = (req, res) => {
-    const id = parseInt(req.params.id);
+    const { id } = req.params;
     const profesorFound = searchById(id, profesores);
-    if (isNaN(id)) {
-        return res.status(400).json({ "Error": "Invalid Parameters" });
-    }
+
     if (!profesorFound) {
-        return res.status(200).json({ "Error": "Profesor not found" });
+        return res.status(404).json({ "Error": "Profesor not found" });
     }
-    const { numeroEmpleado, nombres, apellidos, horaClase } = profesorFound;
-    return res.status(200).json({ numeroEmpleado, nombres, apellidos, horaClase });
+    return res.status(200).json(profesorFound);
 }
 
 module.exports.createProfesor = (req, res) => {
-    const defaultID = profesores.length + 1;
-    const { id = defaultID, numeroEmpleado, nombres, apellidos, horasClase } = req.body;
+    const { id, numeroEmpleado, nombres, apellidos, horasClase } = req.body;
     const exist = searchById(numeroEmpleado, profesores);
-    if (numeroEmpleado == undefined || nombres == undefined || apellidos == undefined || horasClase == undefined) {
-        return res.status(400).json({ "Error": "Invalid Parameters" });
-    }
-    if (horasClase < 0) {
+    if (!validParams(numeroEmpleado, nombres, apellidos, horasClase)) {
         return res.status(400).json({ "Error": "Invalid Parameters" });
     }
     if (exist) {
@@ -47,23 +38,46 @@ module.exports.createProfesor = (req, res) => {
 
 module.exports.updateProfesor = (req, res) => {
     const { id } = req.params;
-    const profesorFound = searchById(id, profesores);
-    if (!profesorFound) {
-        return res.status(404).json({ "Error": "Profesor not found" });
-    }
     const { numeroEmpleado, nombres, apellidos, horasClase } = req.body;
+    const profesorFound = searchById(id, profesores);
+
+    if (!profesorFound) {
+        return res.status(400).json({ "Error": "Profesor not found" });
+    }
+    if (!validParams(numeroEmpleado, nombres, apellidos, horasClase)) {
+        return res.status(400).json({ "Error": "Invalid Parameters" });
+    }
+
     profesores[profesores.indexOf(profesorFound)] = { ...profesorFound, numeroEmpleado, nombres, apellidos, horasClase };
-    res.status(200).json({ numeroEmpleado, nombres, apellidos, horasClase });
+    res.status(200).json({ id, numeroEmpleado, nombres, apellidos, horasClase });
 }
 
 module.exports.deleteProfesor = (req, res) => {
     const { id } = req.params;
     const profesorFound = searchById(id, profesores);
     if (!profesorFound) {
-        return res.status(200).json({ "Error": "Profesor not found" });
+        return res.status(404).json({ "Error": "Profesor not found" });
     }
-    const newProfesores = profesores.filter(prof => prof.id != id);
-    profesores = newProfesores;
+    profesores = profesores.filter(prof => prof.id != id);
     res.status(200).json({ "deleted": profesorFound });
-
 }
+
+// VALIDAR INFORMACION AL CAMBIAR O CREAR UN PROFESOR
+const validParams = (numeroEmpleado, nombres, apellidos, horasClase) => {
+    return testValidNumberWorker(numeroEmpleado) &&
+        testValidNames(nombres) &&
+        testValidNames(apellidos) &&
+        testValidHours(horasClase);
+};
+
+const testValidNames = (str) => {
+    const regexNames = new RegExp(/^[a-zA-ZÀ-ÿ ]+/)
+    return (str != null && regexNames.test(str));
+};
+const testValidNumberWorker = (str) => {
+    const regexNumberWorker = new RegExp(/^\d+/);
+    return (str != null && regexNumberWorker.test(str));
+};
+const testValidHours = (hours) => {
+    return hours != null && hours >= 0;
+};
--- src/routes/profesores.routes.js
+++ src/routes/profesores.routes.js
@@ -2,20 +2,23 @@ const { Router } = require("express");
 const profesorController = require("./controllers/profesores.controller");
 
 const router = Router();
+
 // GET / profesores
 router.get("/", profesorController.getProfesores);
+// POST / profesores
+router.post("/", profesorController.createProfesor);
 
 
 // GET / profesores / { id }
 router.get("/:id", profesorController.getProfesorById);
-
-// POST / profesores
-router.post("/", profesorController.createProfesor);
-
 // PUT / profesores / { id }
 router.put("/:id", profesorController.updateProfesor);
-
 // DELETE / profesores / { id }
 router.delete("/:id", profesorController.deleteProfesor);
+
+// Todo lo demás que no sea /profesores con [GET, POST] => 405
+router.route("/").all(profesorController.methodNotAllowed);
+// Todo lo demás que no sea /profesores/:id con [GET, PUT, DELETE] => 405
+router.route("/:id").all(profesorController.methodNotAllowed);
 
 module.exports = router;
